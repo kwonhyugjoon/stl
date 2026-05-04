89a0f58e670b32e8ccde36ffbc38a4c34541eb08
kwonhyugjoon <kwonhyugjoon58@gmail.com>
2026-05-04 15:39:01 +0900

    STL 5-04일 9주2일차 수업내용 추가

--- 메인.cpp
+++ 메인.cpp
@@ -1,15 +1,17 @@
 //--------------------------------------------------
-// 2026 1학기 STL 월56 화78 4월 28일 (9주 1일)
+// 2026 1학기 STL 월56 화78 4월 04일 (9주 2일)
 //--------------------------------------------------
 // STL 컨테이너 - Containers are objects that store other objects.
 // Sequence Container
 // - array<T, N> - 유일하게 컴파일 타임에 size 결정 - STACK, DATA
-// - vector<T> - dynamic (size) array - free-store에 data 관리
+// - vector<T> - 캐시 히트율이 높아 고속 데이터 처리에 유리
+// - list<T> - 아무대서나 원소 추가/삭제 0(1)
 //--------------------------------------------------
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <algorithm>
-#include <list>			// insert 예시를 위해 추가
+#include <list>
 #include "save.h"
 #include "ZString.h"
 
@@ -21,12 +23,17 @@ int main()
 {
 	save("메인.cpp");
 
-	std::list<ZString> v{ "1", "22", "4444", "55555"};
+	//[문제] 파일에 있는 단어를 list<ZString>에 저장하라.
+	//단어를 사전식 오름차순으로 정렬하라.
+	std::ifstream in("2026년 1학기 STL 월56 화78.txt");
+	if (not in) {
+		std::cout << "파일을 열 수 없습니다." << std::endl;
+		return 20260504;
+	}
 
-	// [문제] "22" 다음에 "333"을 추가하라.
-	관찰 = true;
-	v.emplace(++++v.begin(), "333");
-	관찰 = false;
-	for(const ZString& zs : v)
-		std::cout << zs << std::endl;
+	std::list<ZString> words{ std::istream_iterator<ZString>{in}, {} };
+	words.sort([](const ZString& a, const ZString& b) {
+		// 다음시간에 -> 정렬시간비교
+		lexicographical_compare(a,)
+		});
 }
