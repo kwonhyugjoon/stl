c036af7860cf7c6e4dc53b6d7d9c5fd4c14ccb58
kwonhyugjoon <kwonhyugjoon58@gmail.com>
2026-06-20 11:54:04 +0900

    종강까지 모든 내용 추가

--- 메인.cpp
+++ 메인.cpp
@@ -1,45 +1,62 @@
 //--------------------------------------------------
-// 2026 1학기 STL 월56 화78 6월 02일 (13주 1일)
-// 6/22 시험(15/2) - 제일 마지막 시험
+// 2026 1학기 STL 월56 화78 6월 16일 (15주 1일)
+// 6/22 기말시험(15/2) - 제일 마지막 시험
 //--------------------------------------------------
+// universal reference - range for 에서 range factory를 이용할 때 사용
 // 
+// concept
+// type traits
+// 
+// C++ Constrained Algothm ( Range + Concept )
 //--------------------------------------------------
 #include <iostream>
-#include <map>
-#include <random>
+#include <vector>
+#include <ranges>
 #include <print>
-#include "save.h"
 #include "ZString.h"
+#include "save.h"
 
-extern bool 관찰;			// 관찰하려면 true
+using namespace std;
 
-std::default_random_engine dre;
-std::uniform_int_distribution<int> uid{ 0, 10'000 };
-std::normal_distribution nd{ 0.0, 0.09 };
+extern bool 관찰;
 
-//--------
+template <class T>
+concept 숫자만 = is_integral_v<T> || is_floating_point_v<T>;
+
+template<숫자만 T>
+T add(const T& a, const T& b)
+{
+	return a + b;
+}
+
+class Dog {
+public:
+	Dog() = default;
+	Dog(int n) : n(n) {};
+
+	Dog operator+(const Dog& rhs) const {
+		return n + rhs.n;
+	}
+
+private:
+	int n;
+
+	friend ostream& operator<<(ostream& os, const Dog& dog) {
+		return os << dog.n;
+	}
+};
+
+//----------
 int main()
-//--------
+//----------
 {
 	save("메인.cpp");
 
-	// map - 어디에 쓸 수 있나
-	// 유니폼 분포는 유니폼한가?
-	// 노멀분포를 화면에 출력
+	// [문제] add 함수를 작성하라.
+	cout << add(1, 2) << endl;
+	cout << add(1.2, 3.4) << endl;
+	// concept - 컴파일 타임에 조건을 판단할 수 있음.
 
-	std::map<size_t, size_t> 노멀;
-	for (int i = 0; i < 100'000'000; ++i) {
-		double num = nd(dre);
-		num = num * 5000 + 5000;
-		if (num < 0)
-			num = 0;
-		if (10000 < num)
-			num = 10000;
-
-		++노멀[num / 200];
-	}
-
-	for (auto [구간, 개수] : 노멀) {
-		std::println("[{:2}] - {}", 구간, 개수);
-	}
+	// [질문] 내가 만든 class Dog가 내가 만든 concept 숫자만을 만족하도록 하고 싶다.
+	// cout << add(Dog{ 1 }, Dog{ 2 }) << endl;	// 3이라고 출력하라.
 }
